cf23a681ea40621cf1486df17e4c027d3c6e4991
Wedrig <jackkmorice@gmail.com>
2017-01-27 19:30:25 -0600

    Create page.js

---
+++ page.js
@@ -0,0 +1,4 @@
+//Props to github.com/Bax3n for this code!
+document.body.innerHTML='<div class="nav"><a href="//subtitledgnome.github.io"><li>Subtitledgnome</li></a><a href="//subtitledgnome.github.io/copypasta/home.html"><li>Games/Stuff</li></a></div> \
+<div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//wedrig.github.io/Home.css">'
+
